8aabc55ccffb7ea1769fece705b025c03c9e1a5a
kumait mustafa <kumait@gmail.com>
2024-06-25 00:51:04 +0200

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+// next.config.mjs
 import stylexPlugin from "@stylexjs/nextjs-plugin";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -5,7 +6,12 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
+};
 
 export default stylexPlugin({
   rootDir: __dirname,
